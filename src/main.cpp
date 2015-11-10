ed8b79aff92b57046172b70274605647b5263be6
Jan Stephan <j.stephan@hzdr.de>
2015-11-10 17:30:26 +0100

    Removed pipeline architecture for now

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,11 @@
 #include "Image.h"
 #include "ImageHandler.h"
 
-#include "InputSide.h"
-#include "OutputSide.h"
-#include "Queue.h"
-
-#include "SourceStage.h"
-#include "SinkStage.h"
-#include "Stage.h"
-#include "Pipeline.h"
-
 #include "ImageHandlerPolicies/TIFFHandler.h"
 
 int main()
 {
 	ImageHandler<TIFFHandler> tiff_handler;
+
 	std::cout << "Hello, HZDR!" << std::endl;
 }
