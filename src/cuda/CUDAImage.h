14a4e3e21b2979f8e5b59dcc496ff1561687b6ce
Jan Stephan <j.stephan@hzdr.de>
2015-11-19 18:59:31 +0100

    CUDAImage implementation missing in last commit

---
+++ src/cuda/CUDAImage.h
@@ -0,0 +1,76 @@
+/*
+ * CUDAImage.h
+ *
+ *  Created on: 19.11.2015
+ *      Author: Jan Stephan
+ *
+ *      CUDAImage provides an implementation for the Image class that loads the image into GPU memory.
+ */
+
+#ifndef CUDAIMAGE_H_
+#define CUDAIMAGE_H_
+
+#include <cstddef>
+#include <memory>
+#include <stdexcept>
+
+#ifndef __CUDACC__
+#include <cuda_runtime.h>
+#endif
+
+#include "CUDADeleter.h"
+
+namespace ddafa
+{
+	namespace impl
+	{
+		template <typename Data = float>
+		class CUDAImage
+		{
+			public:
+				using deleter_type = CUDADeleter;
+
+			public:
+				std::unique_ptr<Data> allocate(std::size_t size)
+				{
+					void *ptr = nullptr;
+					cudaError_t err = cudaMalloc(&ptr, size);
+					switch(err)
+					{
+						case cudaErrorMemoryAllocation:
+							throw std::runtime_error("CUDAImage: Error while allocating memory");
+
+						case cudaSuccess:
+						default:
+							break;
+					}
+
+					return std::unique_ptr<Data, deleter_type>(static_cast<Data*>(ptr));
+				}
+
+				void copy(const Data* src, Data* dest, std::size_t size)
+				{
+					cudaError_t err = cudaMemcpy(dest, src, size, cudaMemcpyDeviceToDevice);
+					switch(err)
+					{
+						case cudaErrorInvalidValue:
+							throw std::runtime_error("CUDAImage: Invalid value");
+
+						case cudaErrorInvalidDevicePointer:
+							throw std::runtime_error("CUDAImage: Invalid device value");
+
+						case cudaErrorInvalidMemcpyDirection:
+							throw std::runtime_error("CUDAImage: Invalid memcpy direction");
+
+						case cudaSuccess:
+						default:
+							break;
+					}
+				}
+
+		};
+	}
+}
+
+
+#endif /* CUDAIMAGE_H_ */
